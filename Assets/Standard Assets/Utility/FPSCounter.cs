decd32a523aa54463f8cc03ed7ea9f4c63a44211
Snake <andreacarbonara2014@gmail.com>
2020-04-09 19:54:10 +0200

    aggiunta pianeti

--- Assets/Standard Assets/Utility/FPSCounter.cs
+++ Assets/Standard Assets/Utility/FPSCounter.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnityStandardAssets.Utility
 {
-    [RequireComponent(typeof (UnityEngine.UI.Text))]
+    [RequireComponent(typeof (Text))]
     public class FPSCounter : MonoBehaviour
     {
         const float fpsMeasurePeriod = 0.5f;
@@ -11,13 +12,13 @@ namespace UnityStandardAssets.Utility
         private float m_FpsNextPeriod = 0;
         private int m_CurrentFps;
         const string display = "{0} FPS";
-        private UnityEngine.UI.Text m_GuiText;
+        private Text m_Text;
 
 
         private void Start()
         {
             m_FpsNextPeriod = Time.realtimeSinceStartup + fpsMeasurePeriod;
-            m_GuiText = GetComponent<UnityEngine.UI.Text>();
+            m_Text = GetComponent<Text>();
         }
 
 
@@ -30,7 +31,7 @@ namespace UnityStandardAssets.Utility
                 m_CurrentFps = (int) (m_FpsAccumulator/fpsMeasurePeriod);
                 m_FpsAccumulator = 0;
                 m_FpsNextPeriod += fpsMeasurePeriod;
-                m_GuiText.text = string.Format(display, m_CurrentFps);
+                m_Text.text = string.Format(display, m_CurrentFps);
             }
         }
     }
